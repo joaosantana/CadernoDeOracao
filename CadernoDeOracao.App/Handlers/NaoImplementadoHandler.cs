ae795146828b1374bbb0f0494e44cc21c890d1ce
João Antonio Santana <joaosantana@outlook.com>
2022-02-19 00:00:00 -0300

    adiciona NaoImplementadoHandler.cs

---
+++ CadernoDeOracao.App/Handlers/NaoImplementadoHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace CadernoDeOracao.App.Handlers
+{
+    public class NaoImplementadoHandler
+    {
+        public static void MostrarAviso()
+        {
+            _ = MessageBox.Show(
+                "Esta função ainda não foi implementada.",
+                "Não Implementado",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
