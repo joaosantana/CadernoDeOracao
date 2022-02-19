952b6d4fa728e620735fa1c9918ab6922115e354
João Antonio Santana <joaosantana@outlook.com>
2022-02-18 22:26:44 -0300

    alterações de design

--- CadernoDeOracao.App/FrmPrincipal.Designer.cs
+++ CadernoDeOracao.App/FrmPrincipal.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.SuspendLayout();
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(624, 301);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caderno de Oração";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
